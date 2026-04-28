bf0b93272bf43c84d2f6fd7c534fb1fd9779f9ca
Yuri-Kawahara <yuri44th@icloud.com>
2026-04-28 16:25:59 +0900

    change

--- config.sh
+++ config.sh
@@ -4,21 +4,25 @@
 BASE_DIR="/Users/Documents"
 
 # --- 入力データパス ---
+# 生のFASTQが格納されている場所
 RAW_FASTQ_DIR="${BASE_DIR}/RNA..."
-TRIM_DIR="${RAW_FASTQ_DIR}/trimmed"
+# トリミング済みFASTQの出力先
+TRIM_DIR="${RAW_FASTQ_DIR}/qcresult"
 
 # --- リファレンス・インデックスパス ---
-# STAR インデックスディレクトリ
-STAR_INDEX_MM="${BASE_DIR}/gdc_reference/mm39_star_index"
-STAR_INDEX_HU="${BASE_DIR}/gdc_reference/GRCh38_v49_star_index"
+# GTFファイルのフルパス
+GENCODE_GTF="${BASE_DIR}/Reference/mouse/gencode.vM38.primary_assembly.annotation.gtf"
 
-# GTFファイル (マウス用とヒト用を定義)
-GENCODE_GTF_MM="${BASE_DIR}/Reference/mouse/gencode.vM38.primary_assembly.annotation.gtf"
-GENCODE_GTF_HU="${BASE_DIR}/Reference/human/gencode.v49.primary_assembly.annotation.gtf"
+# STAR インデックスディレクトリ (マウス用とヒト用を定義)
+STAR_INDEX_MM="${BASE_DIR}/gdc_reference/mm39_star_index"
+STAR_INDEX_HU="${BASE_DIR}/gdc_reference/star/GRCh38_v49_star_index"
+
+# Salmon インデックスディレクトリ (必要に応じてこちらも追加可能)
+SALMON_INDEX_DIR="${BASE_DIR}/gdc_reference/salmon/gencode.v36.salmon_index"
 
 # --- 出力先ディレクトリ ---
-STAR_OUTPUT_DIR="${BASE_DIR}/STAR_output/zebra"
-SALMON_OUTPUT_DIR="${BASE_DIR}/SALMON_output/zebra"
+STAR_OUTPUT_DIR="${BASE_DIR}/STAR_output/bulk_202604"
+SALMON_OUTPUT_DIR="${BASE_DIR}/SALMON_output/bulk_202604"
 
 # --- 実行パラメータ ---
 THREADS=16
